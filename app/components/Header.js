c74c56ea83adeb35d13707cfca876b61a141bbcb
arben-djokovic <benidjokovic12@gmail.com>
2025-10-05 20:42:36 +0200

    finished header design: responsible header

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,23 +7,31 @@ import Link from 'next/link';
 
 export default function Header() {
     const [isNavOpen, setIsNavOpen] = React.useState(false);
+    const [isLogged, setIsLogged] = React.useState(false);
 
   return (
-    <header className={`border-b top-0 left-0 bg-white border-gray-300 fixed w-full z-50 ${isNavOpen ? 'h-screen' : 'h-auto'} md:h-auto`}>
-        <div className='flex items-center justify-between px-[5vw] py-4 border-gray-300 border-b'>
+    <header className={`border-b top-0 left-0 bg-white border-gray-300 fixed w-full z-50 ${isNavOpen ? 'h-screen' : 'h-auto'} md:h-auto md:flex justify-between md:px-[3%] md:py-3 md:items-center md:gap-[1%] md:text-sm lg:text-base`}>
+        <div className='flex items-center justify-between px-[5vw] py-4 border-gray-300 border-b md:p-0 md:border-0'>
             <div className='flex items-center gap-2'>
                 <Wrench color='blue' size={32}/>
-                <span className='font-bold text-2xl'>Radnik App</span>
+                <Link href="/" className='font-bold text-2xl text-nowrap'>Radnik App</Link>
             </div>
-            {!isNavOpen ? <Menu onClick={() => setIsNavOpen(!isNavOpen)} className='cursor-pointer hover:text-gray-600' /> : <X onClick={() => setIsNavOpen(!isNavOpen)} className='cursor-pointer hover:text-gray-600' />}
+            <div className='md:hidden'>
+                {!isNavOpen && <Menu onClick={() => setIsNavOpen(!isNavOpen)} className='cursor-pointer hover:text-gray-600' />}
+                {isNavOpen && <X onClick={() => setIsNavOpen(!isNavOpen)} className='cursor-pointer hover:text-gray-600' />}
+            </div>
         </div>
 
-        {isNavOpen && <Nav />}
+        <Nav isNavOpen={isNavOpen} />
 
-        {isNavOpen && <div className='flex flex-col gap-3 py-7 px-2 border-b border-gray-300'>
-            <button className='w-full py-1 border border-gray-300 cursor-pointer hover:bg-gray-100 rounded-md'><Link href="/prijava">Prijavi se</Link></button>
-            <button className='w-full py-1 bg-black text-white border border-black cursor-pointer hover:bg-gray-800 rounded-md'><Link href="/registracija">Registruj se</Link></button>
-        </div>}
+        <div className={`${isNavOpen ? 'block' : 'hidden'} flex flex-col gap-3 py-7 px-2 border-b border-gray-300 md:flex md:border-0 md:flex-row md:p-0 md:gap-[2%]`}>
+            <button className='w-full min-w-[100px] py-1 border border-gray-300 cursor-pointer hover:bg-gray-100 rounded-md'>
+                {!isLogged ? <Link href="/prijava">Prijavi se</Link>: <Link href="/profil">Moj profil</Link>}
+            </button>
+            <button className='w-full min-w-[100px] py-1 bg-black text-white border border-black cursor-pointer hover:bg-gray-800 rounded-md'>
+                {!isLogged ? <Link href="/registracija">Registruj se</Link> : <Link href="/odjava">Odjavi se</Link>}
+            </button>
+        </div>
     </header>
   )
 }
