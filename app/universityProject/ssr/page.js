544cf561cd48f6b4035173797c361187225d1a6b
arben-djokovic <benidjokovic12@gmail.com>
2025-10-22 13:53:22 +0200

    add temporary test page comparing SSR and CSR rendering approaches for university project

--- app/universityProject/ssr/page.js
+++ app/universityProject/ssr/page.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
-export default function page() {
+
+export default async function page() {
+
+  const songs = await fetch('https://repertorify.com/api/songs', { cache: 'no-store' }).then(res => res.json())
+  const genres = await fetch('https://repertorify.com/api/genres', { cache: 'no-store' }).then(res => res.json())
+
   return (
-    <div>page</div>
+    <div>
+      <div className='border border-black'>
+        <h1>songs</h1>
+        <div>
+          {Array.isArray(songs) && songs.length > 0 && songs.map((song) => (
+            <div key={song._id}>
+              {song.title}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='border border-black'>
+        <h1>genres</h1>
+        <div>
+          {Array.isArray(genres) && genres.length > 0 && genres.map((genre) => (
+            <div key={genre._id}>
+              {genre.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
