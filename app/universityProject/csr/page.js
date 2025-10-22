cd76fcf0f51e42e8fcd9624c90290b8bdee31f05
arben-djokovic <benidjokovic12@gmail.com>
2025-10-22 13:59:34 +0200

    testing for uni

--- app/universityProject/csr/page.js
+++ app/universityProject/csr/page.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 
-export default function page() {
+export default function Page() {
   const [songs, setSongs] = useState([])
   const [genres, setGenres] = useState([])
 
