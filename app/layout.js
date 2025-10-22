466e4eaa9e01ad9689d9eb777b01d7f4285a3bb9
arben-djokovic <benidjokovic12@gmail.com>
2025-10-22 13:36:47 +0200

    fix: wrap RootLayout in Suspense to handle useSearchParams warning

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { icons } from "lucide-react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./globals.css";
+import { Suspense } from "react";
 
 export const metadata = {
   title: 'Radnik App',
@@ -16,9 +16,11 @@ export default function RootLayout({ children }) {
     <html lang="en" className="h-full">
       <body className="text-gray-800 h-full bg-white pt-16 md:pt-14">
         <Header />
-        <main className="min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-56px)]">
-          {children}
-        </main>
+        <Suspense fallback={null}>
+          <main className="min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-56px)]">
+            {children}
+          </main>
+        </Suspense>
         <Footer />
       </body>
     </html>
