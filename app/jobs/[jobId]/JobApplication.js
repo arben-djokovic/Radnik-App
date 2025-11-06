974077d0fcb27ffda249370c300e4c8bddf266a3
arben-djokovic <benidjokovic12@gmail.com>
2025-11-06 21:44:49 +0100

    Added accept and reject worker functionallity and fixed responsibility

--- app/jobs/[jobId]/JobApplication.js
+++ app/jobs/[jobId]/JobApplication.js
@@ -1,7 +1,20 @@
+'use client'
 import { Calendar, CircleCheckBigIcon, Euro, StarIcon, TimerIcon, X } from "lucide-react";
 import React from 'react'
 
 export default function JobApplication() {
+    const [approved, setApproved] = React.useState(false);
+    const [rejected, setRejected] = React.useState(false);
+
+    const approveWorker = () => {
+        setApproved(true);
+        setRejected(false);
+    }
+    const rejectWorker = () => {
+        setApproved(false);
+        setRejected(true);
+    }
+
   return (
       <div className="flex flex-col gap-5 border border-gray-300 p-5 rounded-md">
         <div className="flex gap-2 cursor-pointer hover:opacity-80">
@@ -11,7 +24,7 @@ export default function JobApplication() {
                 <p className='text-sm text-blue-500'>
                     <span>Vodoinstalateri</span>
                     <span className='text-black'>{" | "}</span><span>Električari</span><span className='text-black'>{" | "}</span>
-                    <span>Moleri</span><span className='text-black'>{" | "}</span>
+                    <span>Moleri</span>
                 </p>
                 <div className="text-sm flex gap-5 mt-1">
                     <div className='flex gap-1 items-center'>
@@ -24,58 +37,58 @@ export default function JobApplication() {
         </div>
         <div className="text-sm">
             <p className="font-bold">Ponuda:</p>
-            <p>Imam 15 godina iskustva sa servisom klima uređaja. Mogu da dođem narednog utorka oko 10h. Pregledam sve vrste uređaja.</p>
+            <p>Mogu da dođem narednog utorka oko 10h. Pregledam sve vrste uređaja.</p>
         </div>
-        <div className="flex gap-[20%]">
-            <div className="flex items-center gap-1 text-sm">
-                <Calendar size={24} />
+        <div className="flex justify-between gap-2 text-xs md:text-sm md:justify-start md:gap-[15%]">
+            <div className="flex items-center gap-1">
+                <Calendar size={22} />
                 <div>
                     <p>Datum</p>
-                    <p>12. Dec 2024</p>
+                    <p className="">12. Dec 2024</p>
                 </div>
             </div>
-            <div className="flex items-center gap-1 text-sm">
-                <TimerIcon size={24} />
+            <div className="flex items-center gap-1">
+                <TimerIcon size={22} />
                 <div>
                     <p>Vreme</p>
-                    <p>10:00 - 12:00</p>
+                    <p className="">10:00 - 12:00</p>
                 </div>
             </div>
-            <div className="flex items-center gap-1 text-sm">
-                <Euro size={24} />
+            <div className="flex items-center gap-1">
+                <Euro size={22} />
                 <div>
                     <p>Cijena</p>
-                    <p>1000 EUR</p>
+                    <p className="">1000 EUR</p>
                 </div>
             </div>
         </div>
-        <div className="flex gap-2">
-            <button className="w-full bg-green-600 py-1 px-3 rounded-md flex items-center justify-center gap-2 text-white text-center hover:opacity-80">
+        { !approved && !rejected && <div className="flex gap-2">
+            <button onClick={approveWorker} className="w-full bg-green-600 py-1 px-3 rounded-md flex items-center justify-center gap-2 text-white text-center hover:opacity-80">
                 <CircleCheckBigIcon size={24} />
                 <p className="text-sm">Prihvati</p>
             </button>
-            <button className="w-full py-1 px-3 rounded-md text-center border border-red-600 text-red-600 hover:bg-red-50">
+            <button onClick={rejectWorker} className="w-full py-1 px-3 rounded-md text-center border border-red-600 text-red-600 hover:bg-red-50">
                 Odbij
             </button>
-        </div>
+        </div>}
 
         {/* Prihvacen radnik */}
-        {/* <div className="flex gap-3 items-center bg-green-100 text-green-800 py-1 px-3 rounded-md">
+        {approved && <div className="flex gap-3 items-center bg-green-100 text-green-800 py-1 px-3 rounded-md">
             <CircleCheckBigIcon size={24} />
             <div>
                 <p>Prihvaćeno</p>
                 <p className="text-sm">Radnik je obavešten o prihvacanju</p>
             </div>
-        </div> */}
+        </div>}
 
         {/* Odbijen radnik */}
-        {/* <div className="flex gap-3 items-center bg-red-100 text-red-800 py-1 px-3 rounded-md">
+        {rejected && <div className="flex gap-3 items-center bg-red-100 text-red-800 py-1 px-3 rounded-md">
             <X size={24} />
             <div>
                 <p>Odbijeno</p>
                 <p className="text-sm">Radnik je odbijen</p>
             </div>
-        </div> */}
+        </div>}
       </div> 
   )
 }
--- app/jobs/[jobId]/page.js
+++ app/jobs/[jobId]/page.js
@@ -5,7 +5,7 @@ import JobApplications from './JobApplications'
 
 export default function JobPage() {
   return (
-    <div className='flex flex-col justify-center items-center py-10'>
+    <div className='flex flex-col justify-center items-center py-10 px-3'>
         <div className='max-w-4xl flex flex-col gap-5'>
             <JobItem noButtons={true} />
             <JobApply />
